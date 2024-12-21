a09d8466ead0c4411c9c7841b11ce249b173f4ae
Satish Basavaraju <satish.aradhya@gmail.com>
2024-12-21 13:59:28 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,1 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConversionModule } from './conversion/conversion.module';
 
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MulterModule.register({
+      dest: './uploads',
+    }),
+    ConversionModule,
+  ],
+})
+export class AppModule {}
