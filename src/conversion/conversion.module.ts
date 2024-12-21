2cff118d2779f26f2da9ee53a8b3f72251856eea
Satish Basavaraju <satish.aradhya@gmail.com>
2024-12-21 14:00:34 +0530

    Update conversion.module.ts

--- src/conversion/conversion.module.ts
+++ src/conversion/conversion.module.ts
@@ -1,1 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ConversionController } from './conversion.controller';
+import { ConversionService } from './conversion.service';
 
+@Module({
+  controllers: [ConversionController],
+  providers: [ConversionService],
+})
+export class ConversionModule {}
