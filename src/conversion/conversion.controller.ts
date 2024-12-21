f60401c6c5db07be0efde37ef33d00e01f9a1748
Satish Basavaraju <satish.aradhya@gmail.com>
2024-12-21 14:00:12 +0530

    Initial draft conversion.controller.ts

--- src/conversion/conversion.controller.ts
+++ src/conversion/conversion.controller.ts
@@ -1,1 +1,14 @@
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ConversionService } from './conversion.service';
 
+@Controller('conversion')
+export class ConversionController {
+  constructor(private readonly conversionService: ConversionService) {}
+
+  @Post('convert')
+  @UseInterceptors(FileInterceptor('file'))
+  async convertFile(@UploadedFile() file: Express.Multer.File) {
+    return this.conversionService.convertToMarkdown(file);
+  }
+}
